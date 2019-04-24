ce0502c8777541c5ae404b092da181e272daf9e2
Paul Nykiel <paul.nykiel@uni-ulm.de>
2019-04-24 11:15:08 +0200

    Added the JoinRequest

--- Communication/Messages/Request/JoinRequest.cpp
+++ Communication/Messages/Request/JoinRequest.cpp
@@ -1,3 +1,7 @@
+#include <utility>
+
+#include <utility>
+
 /**
  * @file JoinRequest.cpp
  * @author paul
@@ -8,6 +12,30 @@
 #include "JoinRequest.hpp"
 
 namespace communication::messages::request {
+    JoinRequest::JoinRequest(std::string lobby, std::string userName, std::string password,
+                             bool isArtificialIntelligence, std::optional<std::string> mods) :
+                             lobby{std::move(lobby)}, userName{std::move(userName)}, password{std::move(password)},
+                             isArtificialIntelligence{isArtificialIntelligence}, mods{std::move(mods)} {}
+
+    auto JoinRequest::getLobby() const -> std::string {
+        return this->lobby;
+    }
+
+    auto JoinRequest::getUserName() const -> std::string {
+        return this->userName;
+    }
+
+    auto JoinRequest::getPassword() const -> std::string {
+        return this->password;
+    }
+
+    auto JoinRequest::getIsAi() const -> bool {
+        return this->isArtificialIntelligence;
+    }
+
+    auto JoinRequest::getMods() const -> std::optional<std::string> {
+        return this->mods;
+    }
 
     auto JoinRequest::getName() -> std::string {
         return "joinRequest";
