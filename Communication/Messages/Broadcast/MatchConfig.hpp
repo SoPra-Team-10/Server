4c364a71b9568686bc0716ddf22635b142046c81
Paul Nykiel <paul.nykiel@uni-ulm.de>
2019-04-24 11:57:23 +0200

    Added MatchConfig

--- Communication/Messages/Broadcast/MatchConfig.hpp
+++ Communication/Messages/Broadcast/MatchConfig.hpp
@@ -14,11 +14,56 @@
 namespace communication::messages::broadcast {
     class MatchConfig {
     public:
+        MatchConfig() = default;
+
+        MatchConfig(int maxRounds, int playerTurnTimeout, int fanTurnTimeout, int playerPhaseTime, int fanPhaseTime,
+                    int ballPhaseTime, float probGoal, float probThrowSuccess, float probKnockOut, float probFoolAway,
+                    float probCatchSnitch, float probCatchQuaffle, float probWrestQuaffle, float probExtraThinderblast,
+                    float probExtraCleansweep, float probExtraComet, float probExtraNimbus, float probExtraFirebolt,
+                    float probFoulFlacking, float probFoulHaversacking, float probFoulStooging, float probFoulBlatching,
+                    float probFoulSnitchnip, float probFoulElf, float probFoulGoblin, float probFoulTroll,
+                    float probFoulSnitch);
+
+        int getMaxRounds() const;
+        int getPlayerTurnTimeout() const;
+        int getFanTurnTimeout() const;
+        int getPlayerPhaseTime() const;
+        int getFanPhaseTime() const;
+        int getBallPhaseTime() const;
+        float getProbGoal() const;
+        float getProbThrowSuccess() const;
+        float getProbKnockOut() const;
+        float getProbFoolAway() const;
+        float getProbCatchSnitch() const;
+        float getProbCatchQuaffle() const;
+        float getProbWrestQuaffle() const;
+        float getProbExtraThinderblast() const;
+        float getProbExtraCleansweep() const;
+        float getProbExtraComet() const;
+        float getProbExtraNimbus() const;
+        float getProbExtraFirebolt() const;
+        float getProbFoulFlacking() const;
+        float getProbFoulHaversacking() const;
+        float getProbFoulStooging() const;
+        float getProbFoulBlatching() const;
+        float getProbFoulSnitchnip() const;
+        float getProbFoulElf() const;
+        float getProbFoulGoblin() const;
+        float getProbFoulTroll() const;
+        float getProbFoulSnitch() const;
 
         static auto getName() -> std::string;
     private:
-
+        int maxRounds;
+        int playerTurnTimeout, fanTurnTimeout, playerPhaseTime, fanPhaseTime, ballPhaseTime;
+        float probGoal, probThrowSuccess, probKnockOut, probFoolAway, probCatchSnitch, probCatchQuaffle,
+                probWrestQuaffle, probExtraThinderblast, probExtraCleansweep, probExtraComet, probExtraNimbus,
+                probExtraFirebolt, probFoulFlacking, probFoulHaversacking, probFoulStooging, probFoulBlatching,
+                probFoulSnitchnip, probFoulElf, probFoulGoblin, probFoulTroll, probFoulSnitch;
     };
+
+    void to_json(nlohmann::json &j, const MatchConfig &matchConfig);
+    void from_json(const nlohmann::json &j, MatchConfig &matchConfig);
 }
 
 #endif //SERVER_MATCHCONFIG_HPP
