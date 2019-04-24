390dc53f832d9f37eafe5c61e9ad2a47371ac5b7
Paul Nykiel <paul.nykiel@uni-ulm.de>
2019-04-24 16:38:51 +0200

    Added MatchStart

--- Communication/Messages/Broadcast/MatchStart.hpp
+++ Communication/Messages/Broadcast/MatchStart.hpp
@@ -10,15 +10,31 @@
 
 #include <string>
 #include <Lib/json.hpp>
+#include <Communication/Messages/Request/TeamConfig.hpp>
+#include "MatchConfig.hpp"
 
 namespace communication::messages::broadcast {
     class MatchStart {
     public:
+        MatchStart(const MatchConfig &matchConfig, request::TeamConfig leftTeamConfig,
+                   request::TeamConfig rightTeamConfig, std::string leftTeamUserName,
+                   std::string rightTeamUserName);
+        MatchConfig getMatchConfig() const;
+        request::TeamConfig getLeftTeamConfig() const;
+        request::TeamConfig getRightTeamConfig() const;
+        std::string getLeftTeamUserName() const;
+        std::string getRightTeamUserName() const;
 
         static auto getName() -> std::string;
-    private:
 
+    private:
+        MatchConfig matchConfig;
+        request::TeamConfig leftTeamConfig, rightTeamConfig;
+        std::string leftTeamUserName, rightTeamUserName;
     };
+
+    void to_json(nlohmann::json &j, const MatchStart &matchStart);
+    void from_json(const nlohmann::json &j, MatchStart &matchStart);
 }
 
 #endif //SERVER_MATCHSTART_HPP
