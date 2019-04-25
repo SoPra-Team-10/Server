41c104aa14f17f379f1b536a1ea2be260b35402d
Paul Nykiel <paul.nykiel@gmail.com>
2019-04-25 17:33:37 +0200

    Added PauseRequest

--- Communication/Messages/Request/PauseRequest.cpp
+++ Communication/Messages/Request/PauseRequest.cpp
@@ -1,3 +1,5 @@
+#include <utility>
+
 /**
  * @file PauseRequest.cpp
  * @author paul
@@ -11,5 +13,21 @@ namespace communication::messages::request {
 
     auto PauseRequest::getName() -> std::string {
         return "pauseRequest";
+    }
+
+    PauseRequest::PauseRequest(std::string message) : message(std::move(message)) {}
+
+    std::string PauseRequest::getMessage() const {
+        return message;
+    }
+
+    void to_json(nlohmann::json &j, const PauseRequest &pauseRequest) {
+        j["message"] = pauseRequest.getMessage();
+    }
+
+    void from_json(const nlohmann::json &j, PauseRequest &pauseRequest) {
+        pauseRequest = PauseRequest{
+            j.at("message").get<std::string>()
+        };
     }
 }
--- Communication/Messages/Request/PauseRequest.hpp
+++ Communication/Messages/Request/PauseRequest.hpp
@@ -14,11 +14,17 @@
 namespace communication::messages::request {
     class PauseRequest {
     public:
+        PauseRequest(std::string message);
+
+        std::string getMessage() const;
 
         static auto getName() -> std::string;
     private:
-
+        std::string message;
     };
+
+    void to_json(nlohmann::json &j, const PauseRequest &pauseRequest);
+    void from_json(const nlohmann::json &j, PauseRequest &pauseRequest);
 }
 
 #endif //SERVER_PAUSEREQUEST_HPP
