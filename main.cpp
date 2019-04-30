c970982aac2621487fa85a0160dd7190eb887443
Paul Nykiel <paul.nykiel@uni-ulm.de>
2019-04-30 15:44:02 +0200

    Server now implements part of the protocol

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 
-#include <SopraNetwork/WebSocketServer.hpp>
-#include <Communication/MessageHandler.hpp>
-#include <Communication/Messages/Unicast/PrivateDebug.hpp>
-
-void onMessage(std::shared_ptr<communication::MessageHandler> messageHandler, communication::messages::Message, int client) {
-    communication::messages::Message reply{communication::messages::unicast::PrivateDebug{"Hello World!"}};
-    messageHandler->send(reply, client);
-}
+#include <Communication/Communicator.hpp>
 
 int main() {
-    auto messageHandler = std::make_shared<communication::MessageHandler>(8080);
-    messageHandler->onReceive(
-            std::bind(&onMessage, messageHandler, std::placeholders::_1, std::placeholders::_2));
+    communication::Communicator communicator{8080};
+
+
+    std::cout << "Started server!" << std::endl;
 
     while (true);
 }
