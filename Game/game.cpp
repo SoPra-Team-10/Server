d73a5f1d6fdc3528369d8223695779945c39e79b
Björn <bjoern2.petersen@t-online.de>
2019-05-02 18:16:19 +0200

    Erste Sachen und initialer Commit

---
+++ Game/game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bjorn on 02.05.19.
+//
+
+#include "game.h"
---
+++ Game/game.h
@@ -0,0 +1,17 @@
+//
+// Created by bjorn on 02.05.19.
+//
+
+#ifndef SERVER_GAME_H
+#define SERVER_GAME_H
+
+#include <SopraGameLogic/SopraGameModel.h>
+
+class Game {
+public:
+    gameModel::Environment env;
+    
+};
+
+
+#endif //SERVER_GAME_H
