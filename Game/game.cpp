e517b8d9cddfa9233ea026709bac7faa1b6b3c65
Björn <bjoern2.petersen@t-online.de>
2019-05-03 14:08:07 +0200

    Signatur für die KLasse Game erstellt so wie die SIgnatur für alle Methoden

--- Game/game.cpp
+++ Game/game.cpp
@@ -3,3 +3,57 @@
 //
 
 #include "game.h"
+#include "iostream"
+
+
+Game::Game(communication::messages::broadcast::MatchConfig matchConfig,
+           communication::messages::request::TeamConfig teamConfig1,
+           communication::messages::request::TeamConfig teamConfig2,
+           communication::messages::request::TeamFormation teamFormation1,
+           communication::messages::request::TeamFormation teamFormation2)
+           //: env(config(matchConfig.getMaxRounds(),
+           //{matchConfig.getPlayerTurnTimeout(),matchConfig.getFanTurnTimeout(), matchConfig.getPlayerPhaseTime(), matchConfig.getFanPhaseTime(), matchConfig.getBallPhaseTime()},
+           //{matchConfig.getProbFoulStooging(),matchConfig}))
+           {
+
+    /*static std::map<communication::messages::types::Broom, gameModel::Broom > broomMap;
+    broomMap[communication::messages::types::Broom::THINDERBLAST] = gameModel::Broom::Thinderblast;
+    broomMap[communication::messages::types::Broom ::NIMBUS2001] = gameModel::Broom::Nimbus_2001;
+    broomMap[communication::messages::types::Broom ::COMET260] = gameModel::Broom::Comet_260;
+    broomMap[communication::messages::types::Broom ::CLEANSWEEP11] = gameModel::Broom::Cleansweep_11;
+    broomMap[communication::messages::types::Broom ::FIREBOLT] = gameModel::Broom::Firebolt;
+    this->env.team1.seeker.broom = broomMap[teamConfig1.getSeeker().getBroom()];
+    this->env.team1.keeper.broom = broomMap[teamConfig1.getKeeper().getBroom()];
+    this->env.team1.beaters[0].broom = broomMap[teamConfig1.getBeater1().getBroom()];
+    this->env.team1.beaters[1].broom = broomMap[teamConfig1.getBeater2().getBroom()];
+    this->env.team1.chasers[0].broom = broomMap[teamConfig1.getChaser1().getBroom()];
+    this->env.team1.chasers[1].broom = broomMap[teamConfig1.getChaser2().getBroom()];
+    this->env.team1.chasers[2].broom = broomMap[teamConfig1.getChaser3().getBroom()];
+    this->env.team2.seeker.broom = broomMap[teamConfig2.getSeeker().getBroom()];
+    this->env.team2.keeper.broom = broomMap[teamConfig2.getKeeper().getBroom()];
+    this->env.team2.beaters[0].broom = broomMap[teamConfig2.getBeater1().getBroom()];
+    this->env.team2.beaters[1].broom = broomMap[teamConfig2.getBeater2().getBroom()];
+    this->env.team2.chasers[0].broom = broomMap[teamConfig2.getChaser1().getBroom()];
+    this->env.team2.chasers[1].broom = broomMap[teamConfig2.getChaser2().getBroom()];
+    this->env.team2.chasers[2].broom = broomMap[teamConfig2.getChaser3().getBroom()];*/
+
+    std::cout<<"Constructor is called"<<std::endl;
+}
+
+void Game::pause() {
+    std::cout<<"pause() is called"<<std::endl;
+}
+
+void Game::resume() {
+    std::cout<<"resume() is called"<<std::endl;
+}
+
+communication::messages::broadcast::Next Game::getNextActor() {
+    std::cout<<"getNextActor() is called"<<std::endl;
+    return communication::messages::broadcast::Next();
+}
+
+bool Game::executeDelta() {
+    std::cout<<"executeDelta is called"<<std::endl;
+    return false;
+}
--- Game/game.h
+++ Game/game.h
@@ -6,11 +6,31 @@
 #define SERVER_GAME_H
 
 #include <SopraGameLogic/SopraGameModel.h>
+#include <SopraNetwork/Listener.hpp>
+#include <Communication/Messages/Broadcast/MatchConfig.hpp>
+#include <Communication/Messages/Broadcast/Next.hpp>
+#include <Communication/Messages/Request/TeamConfig.hpp>
+#include <Communication/Messages/Request/TeamFormation.hpp>
+
+enum Side{
+    left,right
+};
 
 class Game {
 public:
     gameModel::Environment env;
-    
+    util::Listener<Side > TOListener;
+    util::Listener<Side > GameOverEvent;
+    gameModel::Config config;
+
+
+    Game(communication::messages::broadcast::MatchConfig matchConfig, communication::messages::request::TeamConfig teamConfig1, communication::messages::request::TeamConfig teamConfig2, communication::messages::request::TeamFormation teamFormation1, communication::messages::request::TeamFormation teamFormation2);
+    void pause();
+    void resume();
+    communication::messages::broadcast::Next getNextActor();
+    bool executeDelta();
+
+    static std::map<communication::messages::types::Broom, gameModel::Broom > broomMap;
 };
 
 
