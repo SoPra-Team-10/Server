156d51ddac5462348f4a3f4c003edad164f88904
Björn <bjoern2.petersen@t-online.de>
2019-05-07 19:28:38 +0200

    Check für die Teamformation erstellt

--- Util/ConfigCheck.h
+++ Util/ConfigCheck.h
@@ -10,11 +10,9 @@
 
 namespace configCheck {
 
-    static bool checkTeamConfig(const communication::messages::request::TeamConfig &config, const gameHandling::TeamSide &side);
+    //static bool checkTeamConfig(const communication::messages::request::TeamConfig &config, const gameHandling::TeamSide &side);
 
     bool checkTeamFormation(const communication::messages::request::TeamFormation &formation, const gameHandling::TeamSide &side);
-
-    bool checkPlayerOnField(int x, int y);
 
 }
 
