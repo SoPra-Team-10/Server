93d92534c2411da6a1daea2ce7613e7bd1665682
Björn <bjoern2.petersen@t-online.de>
2019-05-07 18:50:23 +0200

    angefangen mit formationChecks

--- Util/ConfigCheck.cpp
+++ Util/ConfigCheck.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "ConfigCheck.h"
+#include "SopraGameLogic/GameModel.h"
+
+bool configCheck::checkTeamFormation(const communication::messages::request::TeamFormation &formation,
+                                     const gameHandling::TeamSide &side) {
+    if(gameModel::Environment::getCell(formation.getBeater1X(),formation.getBeater1Y()) == gameModel::Cell::OutOfBounds){
+        return false;
+    }
+}
--- Util/ConfigCheck.h
+++ Util/ConfigCheck.h
@@ -12,7 +12,9 @@ namespace configCheck {
 
     static bool checkTeamConfig(const communication::messages::request::TeamConfig &config, const gameHandling::TeamSide &side);
 
-    static bool checkTeamFormation(const communication::messages::request::TeamFormation &formation, const gameHandling::TeamSide &side);
+    bool checkTeamFormation(const communication::messages::request::TeamFormation &formation, const gameHandling::TeamSide &side);
+
+    bool checkPlayerOnField(int x, int y);
 
 }
 
