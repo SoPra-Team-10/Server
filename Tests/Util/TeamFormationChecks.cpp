4957506753e5750608a416c69aef52c5757c277d
Björn <bjoern2.petersen@t-online.de>
2019-05-08 18:15:31 +0200

    Tests für die Teamformation überprüfung fertig

--- Tests/Util/TeamFormationChecks.cpp
+++ Tests/Util/TeamFormationChecks.cpp
@@ -6,6 +6,18 @@
 #include "SopraGameLogic/GameModel.h"
 #include "Util/ConfigCheck.h"
 
-TEST(teamFOrmationCHheck, valid_team_formation){
+TEST(teamFormationCHheck, valid_team_formation){
     EXPECT_TRUE(configCheck::checkTeamFormation(communication::messages::request::TeamFormation{0,4,1,4,2,4,3,4,4,4,5,4,6,4}, gameHandling::TeamSide::LEFT));
+}
+
+TEST(teamFormationCHheck, invalid_team_formation0){
+    EXPECT_FALSE(configCheck::checkTeamFormation(communication::messages::request::TeamFormation{0,0,1,4,2,4,3,4,4,4,5,4,6,4}, gameHandling::TeamSide::LEFT));
+}
+
+TEST(teamFormationCHheck, invalid_team_formation1){
+    EXPECT_FALSE(configCheck::checkTeamFormation(communication::messages::request::TeamFormation{10,4,1,4,2,4,3,4,4,4,5,4,6,4}, gameHandling::TeamSide::LEFT));
+}
+
+TEST(teamFormationCHheck, invalid_team_formation2){
+    EXPECT_FALSE(configCheck::checkTeamFormation(communication::messages::request::TeamFormation{0,4,0,4,2,4,3,4,4,4,5,4,6,4}, gameHandling::TeamSide::LEFT));
 }
